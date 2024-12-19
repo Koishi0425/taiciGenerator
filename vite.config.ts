675cee992048769b993054847f45aef5863d856d
Glmg <koishi0425@gmail.com>
2024-12-19 22:18:10 +0800

    fixed web

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/taichiGenerator/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
   },
